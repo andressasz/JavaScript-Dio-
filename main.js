ce67a0450d28ddba1bba2a670c7e9ac6dcdd8fa4
Andressa Souza <andressash32@gmail.com>
2021-04-18 14:20:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,113 @@
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigada por passar o mouse"
+    elemento.innerHTML = "Obrigada";
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+elemento.innerHTML = "Obrigada";
+}
+
+function load(){
+alert("Página carregada"); //alerta pro carregamento da página
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value); //onchange trocar valores
+}
+/*function clicou(){
+document.getElementById("agradecimento").innerHTML = "Obrigada por clicar"; //inner: pemite colocar textos html aqui, e estilzar
+}
+
+function redirecionar(){
+    window.open("https://www.youtube.com/"); ou
+    window.location.href = "https://www.youtube.com/";
+}
+
+
+//console.log(document.getElementById("obrigada"));
+//alert("Obrigada por clicar!");
+
+
+
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+
+/*
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual a sua idade?")
+console.log(validaIdade(idade));
+
+
+
+
+
+
+/*var d = new Date();
+alert(d.getHours());*/
+
+/*var count;
+for(count=0; count <=5; count++){ //repeição com for
+    alert(count);
+}
+*/
+
+/*var count = 0;
+while(count <= 5){
+    console.log(count);
+    count = count + 1 //ou count ++ (estrutura de reptição)
+}
+
+*/
+
+
+
+
+/*var idade = prompt("Qual a sua idade?")
+
+if (idade >= 18){
+    alert("É maior de idade!");
+    }else{
+    alert("É menor de idade!");
+    }*/
+
+/*var estojo = [{nome: "borracha", cor: "amarela"}, {nome: "cola", cor: "branca"}]
+console.log(estojo)
+alert(estojo[1].nome)*/
+
+
+
+/*var estojo = {nome: "borracha", cor: "amarela"} 
+console.log(estojo.cor)
+alert(estojo.nome)*/
+
+
+
+/*var nome = "Andressa Souza!!!";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+
+alert("Bem vindo ao site da " + nome)
+alert(idade + idade2);
+console.log(nome);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase("Japão", "Brasil"));*/
+
+//var lista = ["tesoura", "estojo", "borracha"]
+//lista.push/*pop*/("uva")  //push põe, e pop retira
+//console.log(lista[0]); //e reverse = ele mostra os ultimos itens que foram codados
+//console.log(lista.toString()[0]); //imprimre a primera letra, no caso o T (tesoura)
+//console.log(lista.join(" - ")); //IMPORTANMTEEEE E ÚTIL
+
